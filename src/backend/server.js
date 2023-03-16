23b92854082d37ec2e7ca7ef689ba91f54cc2db9
Oyexumi <jonathan.oyewumi.a@gmail.com>
2023-03-16 14:01:08 +0100

    Lagt til Express

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,8 +1,21 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://joklatin:kuben@jafar.0uqjftd.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const uri =
+  "mongodb+srv://joklatin:kuben@jafar.0uqjftd.mongodb.net/test";
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+connect();
+
+app.listen(8000, () => {
+  console.log("Server started on port 8000");
 });
